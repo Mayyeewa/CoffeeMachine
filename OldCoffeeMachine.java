1ad48bba37e7b1324849227514b6a0babd242a47
Mayyeewa <81863272+Mayyeewa@users.noreply.github.com>
2021-05-11 12:27:55 -0400

    Add files via upload

---
+++ OldCoffeeMachine.java
@@ -0,0 +1,10 @@
+public class OldCoffeeMachine {
+	
+	public static void main(String[] args) {}
+
+		public void selectA() { System.out.println("A_Selected");}
+		public void selectB() { System.out.println("B_Selected");}
+}
+
+
+
